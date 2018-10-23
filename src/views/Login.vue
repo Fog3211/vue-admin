0be525d6f8c607f5a122a9be986b7a6a1ece0d63
fog3211 <384977954@qq.com>
2018-10-23 23:36:24 +0800

    echarts

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,21 @@
 <template>
-  <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="0px" class="demo-ruleForm login-container" @keyup.native.enter="handleSubmit">
-    <h3 class="title">系统登录</h3>
-    <el-form-item prop="account">
-      <el-input type="text" v-model="ruleForm.account" auto-complete="off" placeholder="账号"></el-input>
-    </el-form-item>
-    <el-form-item prop="password">
-      <el-input type="password" v-model="ruleForm.password" auto-complete="off" placeholder="密码"></el-input>
-    </el-form-item>
-    <el-form-item style="width:100%;">
-      <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit" :loading="logining">登录</el-button>
-    </el-form-item>
-  </el-form>
+    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="0px" class="demo-ruleForm login-container" @keyup.native.enter="handleSubmit">
+        <h3 class="title">系统登录</h3>
+        <el-form-item prop="account">
+            <el-input type="text" v-model="ruleForm.account" auto-complete="off" placeholder="账号"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+            <el-input type="password" v-model="ruleForm.password" auto-complete="off" placeholder="密码"></el-input>
+        </el-form-item>
+        <el-form-item style="width:100%;">
+            <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit" :loading="logining">登录</el-button>
+        </el-form-item>
+        <span class="tip_account tip">账号：admin</span>
+        <span class="tip_password tip">密码：123456</span>
+    </el-form>
 </template>
 
 <script>
-
 import { requestLogin } from "../api/api";
 
 export default {
@@ -45,12 +46,8 @@ export default {
                 password: "123456"
             },
             rules: {
-                account: [
-                    { validator: checkAccount, trigger: "blur" }
-                ],
-                password: [
-                    { validator: checkPassword, trigger: "blur" }
-                ]
+                account: [{ validator: checkAccount, trigger: "blur" }],
+                password: [{ validator: checkPassword, trigger: "blur" }]
             }
         };
     },
@@ -64,15 +61,18 @@ export default {
                         username: this.ruleForm.account,
                         password: this.ruleForm.password
                     };
-                    requestLogin(loginParams).then(data => {                       
+                    requestLogin(loginParams).then(data => {
                         this.logining = false;
                         let { msg, code, user } = data;
                         if (code !== 200) {
                             // console.log("error!!");
-                            this.$message({message: msg,type: "error"});
+                            this.$message({ message: msg, type: "error" });
                         } else {
                             //  console.log("session!!");
-                            sessionStorage.setItem("user",JSON.stringify(user));
+                            sessionStorage.setItem(
+                                "user",
+                                JSON.stringify(user)
+                            );
                             this.$router.push({ path: "/table" });
                         }
                     });
@@ -100,6 +100,17 @@ export default {
         margin: 0px auto 40px auto;
         text-align: center;
         color: #505458;
+    }
+    .tip {
+        display: inline-block;
+        width: 150px;
+        color:dimgrey;
+    }
+    .tip_account {
+        text-align: left;
+    }
+    .tip_password {
+        text-align: right;
     }
 }
 </style>
