2c8903bd4f001dfa66a1114133dee132524b5f11
fog3211 <384977954@qq.com>
2018-11-01 23:53:15 +0800

    用户列表信息mock

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -1,39 +1,36 @@
 <template>
-	<div class="container">
-		<my-header @getcollapsed="showcollapsed"></my-header>
-		<div class="manage_page">
-			<el-row>
-				<el-col :span="collapsed?4:1" class="nav">
-					<el-menu :default-active="defaultActive" router>
-						<el-menu-item index="manage"><i class="el-icon-menu"></i>首页</el-menu-item>
-						<el-submenu index="2">
-							<template slot="title"><i class="el-icon-document"></i>数据管理</template>
-							<el-menu-item index="userList">用户列表</el-menu-item>
-						</el-submenu>
-						<el-submenu index="3">
-							<template slot="title"><i class="el-icon-plus"></i>添加数据</template>
-							<el-menu-item index="addShop">添加商铺</el-menu-item>
-							<el-menu-item index="addGoods">添加商品</el-menu-item>
-						</el-submenu>
-						<el-submenu index="4">
-							<template slot="title"><i class="el-icon-star-on"></i>图表</template>
-							<el-menu-item index="charts">图表</el-menu-item>
-						</el-submenu>
-						<el-submenu index="5">
-							<template slot="title"><i class="el-icon-edit"></i>编辑</template>
-							<el-menu-item index="edit">文本编辑</el-menu-item>
-						</el-submenu>
-					</el-menu>
-				</el-col>
-				<el-col :span="20" class="main-content">
-					<keep-alive>
-						<router-view></router-view>
-					</keep-alive>
-				</el-col>
-			</el-row>
-		</div>
-	</div>
-
+    <div class="container">
+        <my-header @getcollapsed="showcollapsed"></my-header>
+        <div class="manage_page">
+            <el-row>
+                <el-col :span="4" class="nav">
+                    <el-menu :default-active="defaultActive" router>
+                        <el-menu-item index="manage"><i class="el-icon-menu"></i>首页</el-menu-item>
+                        <el-submenu index="2">
+                            <template slot="title"><i class="el-icon-document"></i>数据管理</template>
+                            <el-menu-item index="userList">用户列表</el-menu-item>
+                            <el-menu-item index="addGoods">用户管理</el-menu-item>
+                        </el-submenu>
+                        <el-submenu index="3">
+                            <template slot="title"><i class="el-icon-star-on"></i>图表</template>
+                            <el-menu-item index="charts">图表</el-menu-item>
+                        </el-submenu>
+                        <el-submenu index="4">
+                            <template slot="title"><i class="el-icon-edit"></i>编辑</template>
+                            <el-menu-item index="edit">文本编辑</el-menu-item>
+                        </el-submenu>
+                    </el-menu>
+                </el-col>
+                <el-col :span="20" class="main-content">
+                    <transition name="fade" mode="out-in">
+                        <keep-alive>
+                            <router-view></router-view>
+                        </keep-alive>
+                    </transition>
+                </el-col>
+            </el-row>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -85,5 +82,13 @@ export default {
             }
         }
     }
+}
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
 }
 </style>
--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -1,15 +1,78 @@
 <template>
-    <div>
-        this is userlist
-    </div>
-</template>
+    <section>
+        <!--工具条-->
+        <el-col :span="24" class="toolbar">
+            <el-form :inline="true">
+                <el-form-item>
+                    <el-input v-model="name" placeholder="请输入姓名"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="getUser">查询</el-button>
+                </el-form-item>
+            </el-form>
+        </el-col>
 
+        <!--列表-->
+        <template>
+            <el-table :data="users" highlight-current-row border stripe v-loading="loading" class="table-header" :header-cell-style="{background:'#EBEEF5'}">
+                <el-table-column type="index" width="60">
+                </el-table-column>
+                <el-table-column prop="name" label="姓名" width="120" sortable>
+                </el-table-column>
+                <el-table-column prop="sex" label="性别" width="100" :formatter="formatSex" sortable>
+                </el-table-column>
+                <el-table-column prop="age" label="年龄" width="100" sortable>
+                </el-table-column>
+                <el-table-column prop="birth" label="生日" width="120" sortable>
+                </el-table-column>
+                <el-table-column prop="addr" label="地址" min-width="180" sortable>
+                </el-table-column>
+            </el-table>
+        </template>
+
+    </section>
+</template>
 <script>
-    export default {
-        
+import { getUserList } from "../api/api";
+export default {
+    data() {
+        return {
+            name:'',
+            loading: false,
+            users: []
+        };
+    },
+    methods: {
+        //性别显示转换
+        formatSex(row, col) {
+            return row.sex === 1 ? "男" : "女" ;
+        },
+        //获取用户列表
+        getUser: function() {
+            let para = {
+                name: this.name
+            };
+            this.loading = true;
+            getUserList(para).then(res => {
+                this.users = res.data.users;
+                this.loading = false;
+            });
+        }
+    },
+    mounted() {
+        this.getUser();
     }
+};
 </script>
 
 <style scoped>
-
+.toolbar {
+    background: #f2f2f2;
+    padding: 15px 15px 0 15px;
+    margin: 10px 0px;
+}
+.table-header {
+    width: 100%;
+    margin-left: 10px;
+}
 </style>
